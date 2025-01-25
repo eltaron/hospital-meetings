f093b7bc6d24306ab4f64b9bd2b98dbc2422a042
unknown <eltaroon1082000@gmail.com>
2025-01-25 11:58:52 +0200

    last

---
+++ js/loading_page.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    $(document.getElementById("layout")).fadeOut(1000);
+    $(document.getElementById("main_page")).fadeIn(1000);
+}
+
+// $(window).load(function(){
+
+// })
